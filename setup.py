9e5df3e0c355c34015d9aff506d9f7f45310594c
kunal15cr <kunal15cr@gmail.com>
2025-11-23 13:27:10 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Chrome extension that instantly analyzes the sentiment of YouTube comments to show positive, negative, or neutral reactions at a glance.',
+    author='kunal patil',
+    license='',
+)
